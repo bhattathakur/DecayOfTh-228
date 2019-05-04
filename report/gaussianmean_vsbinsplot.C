5bd3b0c1f42dc063c7198b834fc0e53ad17ad0bf
Thakur Bhatta <thakur.bhatta@coyotes.usd.edu>
2019-05-04 18:24:24 -0500

    creation of final modified plot

--- report/gaussianmean_vsbinsplot.C
+++ report/gaussianmean_vsbinsplot.C
@@ -10,8 +10,30 @@
   graph->Draw();
   graph->Fit("pol1");
   graph->SetMarkerSize(2);
-  gStyle->SetOptFit(0011);//SetOptFit(pcev), p->prob ,c->chi square/#df, e->error,v->value of parameters when e=1,v=1, SetOptFit(1)=SetOptFit(111),v=1->only non fixed parameters, v=2->all paramters
+  graph->GetYaxis()->SetTitleOffset(1.5);
+  //TPaveStats * stats1 = (TPaveStats*)graph->GetListOfFunctions()->FindObject("stats");
+  //stats1->SetX1NDC(0.12); stats1->SetX2NDC(0.32); stats1->SetY1NDC(0.75);
+  gStyle->SetOptFit(0);//SetOptFit(pcev), p->prob ,c->chi square/#df, e->error,v->value of parameters when e=1,v=1, SetOptFit(1)=SetOptFit(111),v=1->only non fixed0 parameters, v=2->all paramters
+
+  //Trying to modify the statbox using TPaveStat
+  TPaveText * pt=new TPaveText(0.75,0.63,0.90,0.73,"NDC");
+  //  pt->SetNDC();
+  double par0=graph->GetFunction("pol1")->GetParameter(0);
+  double par1=graph->GetFunction("pol1")->GetParameter(1);
+  double par0error=graph->GetFunction("pol1")->GetParError(0);
+  double par1error=graph->GetFunction("pol1")->GetParError(1);
+  
+  
+  pt->AddText("Parameters");
+  pt->AddText(Form("p0      \t%f#pm%f",par0,par0error));
+  pt->AddText(Form("p1      \t%f#pm%f",par1,par1error));
+  
+  gPad->Modified();
+  gPad->Update();
+
   graph->Draw();
-  c->Update();
+    pt->Draw();
+    c->Update();
+    c->SaveAs("meanvsbin.pdf");
   
 }
